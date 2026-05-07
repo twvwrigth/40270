75ef661d14fe00b8dcc217b5165c2d5b8c351ee3
twvwrigth <akiramtz341@gmail.com>
2026-05-07 15:18:44 -0600

    Tarea Calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,63 +1,69 @@
-let botones = document.getElementsByClassName("numeros");
-let resultado = document.getElementById("resultado");
-let operadores = document.getElementsByClassName("Operador");
-let param;
-let operacion;
-
+let botones = document.getElementsByClassName("numeros")
+let resultado = document.getElementById("resultado")
 
 for (const key in botones) {
     if (!Object.hasOwn(botones, key)) continue;
+
     const boton = botones[key];
     console.log(boton)
-    boton.addEventListener("click", pintar);
+    boton.addEventListener("click", pinta)
+
 }
 
+function pinta(e) {
+    resultado.value += e.target.innerText
+}
+
+let Ce = document.getElementById("bCe")
+let operadores = document.getElementsByClassName("operador")
+let prm, operacion
 for (const key in operadores) {
-
     if (!Object.hasOwn(operadores, key)) continue;
+
     const boton = operadores[key];
-    if (boton.innerText == "CE"){
-        boton.addEventListener("click", limpiar);
+    if (boton.innerText == "Ce") {
+        boton.addEventListener("click", limpiar)
+    } else if (boton.innerText == "+") {
+        boton.addEventListener("click", operar)
+    } else if (boton.innerText == "-") {
+        boton.addEventListener("click", operar)
+    } else if (boton.innerText == "*") {
+        boton.addEventListener("click", operar)
+    } else if (boton.innerText == "/") {
+        boton.addEventListener("click", operar)
+    } else if (boton.innerText == "=") {
+        boton.addEventListener("click", igual)
+    } else {
+        boton.addEventListener("click", pintar2)
     }
 
-    if (boton.innerText == "+"){
-        boton.addEventListener("click", suma);
-    }
-    if (boton.innerText == "="){
-        boton.addEventListener("click", igual);
-    }
-    
-    else 
-        boton.addEventListener("click", pintar2);
-}
 
-function pintar(e) {
-    //console.log(e.target.classname);
-    resultado.value += e.target.innerText;
-}
-
-function limpiar() {
-    resultado.value = "";
 }
 
 function pintar2(e) {
-    console.log(e.target.innerText);
+    console.log(e.target.innerText)
 }
 
-function suma(e){
+function limpiar() {
+    resultado.value = ""
+}
+
+function operar(e) {
     prm = resultado.value
     operacion = e.target.innerText
     resultado.value = ""
-    e.target.style.backgroundColor = "red";
+    e.target.style.backgroundColor = "red"
 }
 
-function igual(){
+function igual() {
     let prm2 = resultado.value
-    if (operacion == "+")
+    if (operacion == "+") {
         resultado.value = parseInt(prm) + parseInt(prm2)
+    } else if (operacion == "-") {
+        resultado.value = parseInt(prm) - parseInt(prm2)
+    } else if (operacion == "*") {
+        resultado.value = parseInt(prm) * parseInt(prm2)
+    } else if (operacion == "/") {
+        resultado.value = parseInt(prm) / parseInt(prm2)
+    }
 }
-
-borrar.addEventListener("click", limpiar);
-sumar.addEventListener("click", suma);
-console.log(operadores);
-
