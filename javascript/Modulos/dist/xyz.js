972c09bbc0029f8b90a72c373a726b722512d4f7
twvwrigth <akiramtz341@gmail.com>
2026-05-20 16:55:53 -0600

    Produccion js

---
+++ javascript/Modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(()=>{function n(o){return console.log("y:"+o),"fin y"}function r(o){return n("x"),console.log("x:"+o),"fin x"}function l(o){return"fin xx"}function i(o){return console.log("z:"+o),"fin z"}console.log(r("abc"));console.log(l("def"));console.log(n("ghi"));console.log(i("jkl"));})();
+//# sourceMappingURL=xyz.js.map
